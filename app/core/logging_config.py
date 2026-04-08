3818be29a853da2c12f378928d20c63348e0aae0
26harvintilavat <tilavatharvin@gmail.com>
2026-04-08 19:20:02 +0530

    add centralized logging configuration

--- app/core/logging_config.py
+++ app/core/logging_config.py
@@ -3,6 +3,6 @@ from app.core.config import settings
 
 def setup_logging() -> None:
     logging.basicConfig(
-        level=(logging, settings.LOG_LEVEL.upper(), logging.INFO),
-        format="%(asctime)s | %(levelname)s | %(name)s | %(messsage)s",
+        level=getattr(logging, settings.LOG_LEVEL.upper(), logging.INFO),
+        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
     )
